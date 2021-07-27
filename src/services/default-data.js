91ed5c18aa335fa01b50bdc7c69c788dd0a7a0d6
Габриелов Максим <gabrielovma@edu.mos.ru>
2021-07-28 01:26:50 +0300

    comleted first task

---
+++ src/services/default-data.js
@@ -0,0 +1,22 @@
+export const quantityElements = 7
+
+export const rectangle = {
+  width: 50,
+  height: 30
+}
+
+export const circle = {
+  radius: 20
+}
+
+export const star = {
+  points: 6,
+  radius1: 20,
+  radius2: 40
+}
+
+export const triangle = {
+  firstPoint: [40, 90],
+  secondPoint: [90, 40],
+  thirdPoint: [140, 90]
+}
